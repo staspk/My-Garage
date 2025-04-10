cebf7e4667f4ad7a050e0883167d9c8d6c830d8e
Stanislav Kozubenko <staspk@gmail.com>
2025-04-10 13:00:22 -0700

    c++ physics sim moved to Garage

---
+++ physics_sim/camera.cpp
@@ -0,0 +1,27 @@
+#ifndef CAMERA_CLASS_H
+#define CAMERA_CLASS_H
+
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+#include<glm/glm.hpp>
+#include<glm/gtc/matrix_transform.hpp>
+#include<glm/gtc/type_ptr.hpp>
+#include<glm/gtx/rotate_vector.hpp>
+#include<glm/gtx/vector_angle.hpp>
+
+
+class Camera
+{
+public:
+	glm::vec3 position;
+	glm::vec3 orientation;
+	glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+
+	int width;
+	int height;
+
+	float speed = 0.1f;
+	float sensitivity = 100.0f;
+};
+
+#endif
---
+++ physics_sim/main.cpp
@@ -0,0 +1,74 @@
+#include <glad/glad.h> 
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+GLFWwindow* glfwStart();
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+
+int main(void)
+{
+    GLFWwindow* window = glfwStart();
+    if (!window) return -1;
+
+
+    int ticks = 0;
+    while (!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+        ticks++;
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+GLFWwindow* glfwStart()
+{
+    if (!glfwInit()) {
+        std::cerr << "Failed to init GLFW!" << std::endl;
+        return nullptr;
+    }
+
+    glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
+    GLFWwindow* window = glfwCreateWindow(640, 480, "3D OpenGl", NULL, NULL);
+
+    if (!window) {
+        glfwTerminate();
+        std::cerr << "Failed to create GLFW window!" << std::endl;
+        return nullptr;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        glfwDestroyWindow(window);
+        glfwTerminate();
+        std::cerr << "Failed to init GLAD!" << std::endl;
+        return nullptr;
+    }
+
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    return window;
+}
