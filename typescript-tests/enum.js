b51baa26086928419d59ecbef7a12ca8426756e0
Stanislav Kozubenko <staspk@gmail.com>
2025-09-30 14:14:45 -0700

    Enum Transpilation Test

---
+++ typescript-tests/enum.js
@@ -0,0 +1,13 @@
+var Direction;
+(function (Direction) {
+    Direction["North"] = "North";
+    Direction["East"] = "East";
+    Direction["South"] = "South";
+    Direction["West"] = "West";
+})(Direction || (Direction = {}));
+
+var Numbers;
+(function (Numbers) {
+    Numbers[Numbers["One"] = 1] = "One";
+    Numbers[Numbers["Two"] = 2] = "Two";
+})(Numbers || (Numbers = {}));
---
+++ typescript-tests/enum.ts
@@ -0,0 +1,11 @@
+enum Direction {
+    North = "North",
+    East = "East",
+    South = "South",
+    West = "West"
+}
+
+enum Numbers {
+    One = 1,
+    Two = 2
+}
