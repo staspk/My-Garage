b8bed13001cc46773406a528e0d943bdb93946e4
Stanislav Kozubenko <staspk@gmail.com>
2025-09-30 15:43:33 -0700

    eg: extending enums via Declation Merging

---
+++ typescript-tests/extending-enums-via-declaration-merging.ts
@@ -0,0 +1,11 @@
+enum MirrorOption {
+    True  = 'true',
+    False = 'false',
+    Null  = 'null'
+}
+
+namespace MirrorOption {
+    export function toString() {
+        return "MirrorOption";
+    }
+}
